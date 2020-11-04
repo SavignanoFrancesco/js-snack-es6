bcc0d568d8a6978920a5a98bb3db5fe7d9a309de
Francesco <francesco.savignano96@gmail.com>
2020-11-04 16:36:45 +0100

    snack2-es6: aggiunti commenti

--- js-snack2-es6/main.js
+++ js-snack2-es6/main.js
@@ -4,13 +4,17 @@
 
 $(document).ready(() => {
 
+    //inizializzo un array
     const arr1 = [1,2,3,4,5,6,7,8,9,10];
 
+    //chiedo all'utente di inserire due numeri
     let num1 = parseInt(prompt('Inserisci primo numero'));
     let num2 = parseInt(prompt('Inserisci secondo numero'));
 
+    //filtro il primo array
     const arr2 = arr1.filter((element, index) => {
 
+        //e copio solo gli elementi tra gli indici scelti dall'utente(num-1 perchè l'array va da 0 a 9)
         if ((index >= (num1 - 1)) && (index <= (num2 - 1))) {
             return true;
         }
